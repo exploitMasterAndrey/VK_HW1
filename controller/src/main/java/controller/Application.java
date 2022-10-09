82989e4ac677ee3971b44cdbf5b7461037298c90
exploitMasterAndrey <mister.andrey-steam@yandex.ru>
2022-10-09 16:57:49 +0300

    added console args

--- controller/src/main/java/controller/Application.java
+++ controller/src/main/java/controller/Application.java
@@ -2,6 +2,6 @@ package controller;
 
 public class Application {
     public static void main(String[] args) {
-        System.out.println(Library.getJsonSerialized(LibraryFactory.getLibrary().getBooksByAuthor("Pushkin A.S.")));
+        System.out.println(Library.getJsonSerialized(LibraryFactory.getLibrary().getBooksByAuthor(args[0])));
     }
 }
